488670a1f7ed5375fdb54822ed1275abc284b2b2
Nassim-Boutaleb <nassim.boutaleb@edu.ece.fr>
2021-04-05 03:12:59 +0200

    basic behavior OK

---
+++ Assets/Scripts/FunChimie/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private GameObject HydroAtome1;
+    private GameObject HydroAtome2;
+    private GameObject OxyAtome;
+
+    private bool good1 = false;
+    private bool good2 = false;
+    private bool good3 = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        HydroAtome1 = GameObject.Find("Hydro");
+        good1 = HydroAtome1.GetComponent<NewDD>().good;
+
+        OxyAtome = GameObject.Find("Oxy");
+        good2 = OxyAtome.GetComponent<NewDD>().good;
+
+        HydroAtome2 = GameObject.Find("Hydro (2)");
+        good3 = HydroAtome2.GetComponent<NewDD>().good;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        good1 = HydroAtome1.GetComponent<NewDD>().good;
+
+        good2 = OxyAtome.GetComponent<NewDD>().good;
+
+        good3 = HydroAtome2.GetComponent<NewDD>().good;
+        //Debug.Log("OHEY");
+        if (good1 && good2 && good3) {
+            Debug.Log("WIN");
+        }
+    }
+}
---
+++ Assets/Scripts/FunChimie/NewDD.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+ 
+class NewDD : MonoBehaviour
+{
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+    public bool good = false;
+ 
+   
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = mouseOverColor;
+        //Debug.Log("OPD");
+    }
+ 
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+ 
+    void OnMouseDown()
+    {
+        //Debug.Log("OPDD");
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+ 
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+ 
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) 
+	{
+		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("Hydro") && gameObject.CompareTag ("HydroAtome"))
+		{
+			Debug.Log("HHOK");
+            good = true;
+		}
+        if (other.gameObject.CompareTag ("Oxy") && gameObject.CompareTag ("OxyAtome"))
+		{
+			Debug.Log("HHOK");
+            good = true;
+		}
+	}
+
+    void OnTriggerExit(Collider other) 
+	{
+		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("Hydro") &&  gameObject.CompareTag ("HydroAtome"))
+		{
+			Debug.Log("Not OK");
+            good=false;
+            // désactiver (ou détruire éventuellment) le pick up 
+            //other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			//count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			//SetCountText ();
+		}
+        if (other.gameObject.CompareTag ("Oxy") &&  gameObject.CompareTag ("OxyAtome"))
+		{
+			Debug.Log("Not OK");
+            good=false;
+		}
+	}
+}
