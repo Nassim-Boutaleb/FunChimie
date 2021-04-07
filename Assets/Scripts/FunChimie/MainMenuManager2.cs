3458dafd36e13c20ec159e9a30cf4a360295c5f6
Nassim-Boutaleb <nassim.boutaleb@edu.ece.fr>
2021-04-07 11:49:40 +0200

    Menu Youssouph

---
+++ Assets/Scripts/FunChimie/MainMenuManager2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager2 : MonoBehaviour
+{
+   public void PlayGame ()
+   {
+  
+      Debug.Log("Play");
+      SceneManager.LoadScene(1);
+      //UnityEngine.SceneManagement.SceneManager.LoadScene("FunChimie", LoadSceneMode.Single);
+
+   }
+
+   public void QuitGame ()
+   {
+   	Debug.Log("QUIT!");
+   	Application.Quit();
+   }   
+}
